9a53eb2ce1c3d09c87a1aeae7f4f5ea9d50f8fcb
marmooo <marmooo@users.noreply.github.com>
2025-04-06 01:50:55 +0900

    bump bootstrap from 5.3.3 to 5.3.5

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "2025-01-26 02:00";
+const CACHE_NAME = "2025-04-06 01:35";
 const urlsToCache = [
   "/talk-abc/",
   "/talk-abc/index.js",
@@ -9,7 +9,7 @@ const urlsToCache = [
   "/talk-abc/kohacu.webp",
   "/talk-abc/favicon/favicon.svg",
   "https://marmooo.github.io/yomico/yomico.min.js",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/chart.js@4.2.1/dist/chart.umd.js",
 ];
 
